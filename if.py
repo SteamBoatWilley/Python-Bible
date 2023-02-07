4d7df61f97884846735a418b4b796ded083c501b
Kevin Gerovac <kevintgerovac@gmail.com>
2023-02-07 14:34:26 -0600

    if statement practice

---
+++ if.py
@@ -0,0 +1,9 @@
+num1 = 109
+num2 = 109
+
+if num1 > num2:
+    print("num1 is bigger than num2")
+elif num2 > num1 :
+    print("num2 is bigger than num1")
+else:
+    print("Both numbers are equal")
