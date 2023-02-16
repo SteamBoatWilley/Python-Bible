f6e7b32661cbff214a81f31717c759873872efc9
Kevin Gerovac <kevintgerovac@gmail.com>
2023-02-15 19:58:11 -0600

    Create Keyword argument script

---
+++ KeywordArguments.py
@@ -0,0 +1,6 @@
+def about(name, age, likes):
+    sentence = "Meet {}! They are {} years old and they likes {}".format(name, age, likes)
+    return sentence
+
+answer = about("Jack", 23, "Python")
+print(answer) #added the answer function so i could print it, otherwise it is fine just cannot run without that. 
